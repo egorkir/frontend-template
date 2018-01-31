4269d84652d49b56a264cf9f00bfb8558647f3ac
deeppines <atheus92@gmail.com>
2018-01-31 19:38:54 +0300

    add global export

--- source/static/scripts/common.js
+++ source/static/scripts/common.js
@@ -4,3 +4,8 @@
 
 import debounce from './functions/debounce';
 import responsiveIframe from './functions/responsive-iframe';
+
+export {
+    debounce as debounce,
+    responsiveIframe as responsiveIframe
+};
